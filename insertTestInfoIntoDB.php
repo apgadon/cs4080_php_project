11a588897cdb79592bdb3ca147725405e2984946
Derek Yee <dereky007@gmail.com>
2019-04-27 22:06:52 -0700

    Add files via upload
    added rachael test information (the guinea pig)

---
+++ insertTestInfoIntoDB.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "phpproject_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO userInfo (firstName, lastName, email,password)
+VALUES ('Rachael', 'Shima', 'reshima@cpp.edu', '12345')";
+
+
+
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "added successfully";
+} else {
+    echo "Error KYS " . $conn->error;
+}
+
+$conn->close();
+?>
