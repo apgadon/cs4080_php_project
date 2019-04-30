2c6539a04d2e3c71aee81865bef260237108e414
Derek Yee <dereky007@gmail.com>
2019-04-29 17:16:04 -0700

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -1,19 +1,50 @@
-<div class="container">
-<?php if (isset($_SESSION['userid'])) { ?>
-<div class="row">
-<div class="navbar-collapse gallery">
-<ul>
-<?php
-$sql_query="SELECT id, user_id, image_title, image_description, image_name FROM user_gallery WHERE user_id='".$_SESSION["userid"]."'";
-$resultset = mysqli_query($conn, $sql_query) or die("database error:". mysqli_error($conn));
-while($rows = mysqli_fetch_array($resultset) ) { ?>
-<li>
-<a href="http://localhost/phpzag/create-dynamic-image-gallery-with-jquery-php-mysql/uploads/<?php echo $rows["image_name"]; ?>" data-lightbox="<?php echo $_SESSION['userid']; ?>" data-title="<?php echo $rows["image_title"]; ?>"><img
-src="https://localhost/phpzag/create-dynamic-image-gallery-with-jquery-php-mysql/uploads/<?php echo $rows["image_name"]; ?>" class="images" width="200" height="200"></a>
-</li>
-<?php } ?>
-</ul>
+<?php 
+session_start();
+?>
+<title>phpzag.com : Gallery! </title>
+
+<div class="container">	
+	<h2>Create Dynamic Image Gallery with jQuery, PHP & MySQL</h2>	
+	<div class="row">
+	<div class="collapse navbar-collapse" id="navbar1">
+		<ul class="nav navbar-nav navbar-left">
+			<?php if (isset($_SESSION['userid'])) { ?>
+			<li><p class="navbar-text"><strong>Welcome!</strong> You're signed in as <strong><?php echo $_SESSION['name']; ?></strong></p></li>
+			<br>
+			<li><a href="upload.php"><strong>Upload More Images in Gallery</strong></a></li>
+			<li><a href="logout.php">Log Out</a></li>
+			<?php } else { ?>
+			<li><p class="navbar-text">You are Logged Out!</p></li><br>
+			<li><a href="logIn.php">Login</a></li>				
+			<?php } ?>
+		</ul>
+	</div>	
+	</div>
+	<?php if (isset($_SESSION['userid'])) { ?>
+		<div class="row">
+			<div class="navbar-collapse gallery">			
+				<ul>
+					<?php	
+					$servername = "localhost";
+					$username = "root";
+					$password = "";
+					$dbname = "phpproject_database";
+
+					// Create connection
+					$conn = new mysqli($servername, $username, $password, $dbname);		
+					$sql_query="SELECT id, user_id,title, description, image_name FROM imagedescription WHERE user_id='".$_SESSION["userid"]."'";
+					$resultset = mysqli_query($conn, $sql_query) or die("database error:". mysqli_error($conn));
+					while($rows = mysqli_fetch_array($resultset) ) { ?>
+					<li>					
+						<a href="http://localhost/phpProject/imageUploads/<?php echo $rows["image_name"]; ?>" data-lightbox="<?php echo $_SESSION['userid']; ?>" data-title="<?php echo $rows["image_title"]; ?>"><img
+				src="http://localhost/phpProject/imageUploads/<?php echo $rows["image_name"]; ?>" class="images" width="200" height="200"></a>
+					</li>
+					<?php } ?>
+				</ul>			
+			</div>
+		</div>
+	<?php } ?>
+	
+	
 </div>
-</div>
-<?php } ?>
-</div>
+
